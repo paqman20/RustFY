7813a9329e87b3cda977f0dbd709ec96a43134c5
Petr Quinn <petrquinn71@protonmail.com>
2023-10-13 07:51:06 -0400

    Successsfully fixed hashing import
    How ever now I have this wonderful runtime error. The stack overflows for main. It is beacuse I am returning the variable of the sha256 has back to the main function. I will have to to move the String to the heap to stop that from happening. However I have literally no idea how to do that but this is progress.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use reqwest::{Client, Error};
 use serde::{Deserialize, Serialize};
 use std::error::Error as StdError;
+use std::{string, vec};
+use sha2::{Sha256, Digest};
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+
 
 // Json Structures
 #[derive(Serialize, Deserialize, Debug)]
@@ -16,16 +21,15 @@ async fn main() -> Result<(), Box<dyn StdError>> {
     // Construct the URL for the authentication
     let client_id = "f5a249051b84495f80f9988b9d1c8b90";
     let client_secret = "4a76f53959d14f1b84a779eb4b44de91";
-    let authentication_struct = fetch_authentication(client_id, client_secret).await?;
-
-    println!("{:?}", authentication_struct.access_token);
+//    let authentication_struct = fetch_authentication(client_id, client_secret).await?;
+//    println!("{:?}", authentication_struct.access_token);
+    let random_string = generate_random_string();
+    let code_challeneger = generate_code_verifier();
+    println!("{}", code_challeneger);
     Ok(())
 }
 
-async fn fetch_authentication(
-    client_id: &str,
-    client_secret: &str,
-) -> Result<Tokens, Box<dyn StdError>> {
+async fn fetch_authentication_client_credentials(client_id: &str, client_secret: &str,) -> Result<Tokens, Box<dyn StdError>> {
     let body = format!(
         "grant_type=client_credentials&client_id={}&client_secret={}",
         client_id, client_secret,
@@ -43,4 +47,21 @@ async fn fetch_authentication(
         .await?;
 
     Ok(authentication_token)
+}
+
+fn generate_code_verifier() -> String {
+    let sha256 = Sha256::new;
+    let code_verifer = generate_code_verifier();
+    let code_challenger = sha256().finalize();
+    
+    return format!("{:x}", code_challenger); 
+}
+
+fn generate_random_string() -> String {
+    let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect();
+ return rand_string
 }
